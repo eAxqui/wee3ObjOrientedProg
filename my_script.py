1f9f169e848200a937e23fc7bcf371b70414b402
jericacilo <john.acilo@edu.sait.ca>
2025-09-18 15:45:32 -0600

    init

---
+++ my_script.py
@@ -0,0 +1,26 @@
+# Third Week Script Demo
+
+""" Working on variables and operators """
+
+x = 22 #integer
+floot = 99.9 #Float
+team = "Calgary Flames" #String
+is_flames_awesome = True #Bool
+is_oilers_awesome = False #Bool
+my_new_var = 23 # Int
+
+# Printing values of variables
+print(x)
+print(floot)
+print(team)
+print(is_flames_awesome)
+print(is_oilers_awesome)
+
+# Printing the data types
+print(type(x))
+print(type(floot))
+print(type(team))
+print("This statement is 100% true.", type(is_flames_awesome))
+print(type(is_oilers_awesome))
+
+
